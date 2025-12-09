a6632e63c8cc2fe299df7b4ed3c1cbfb1584de6f
Marcelo Raineri <mraineridev@gmail.com>
2025-12-08 23:12:17 -0300

    feat: enhance ClassPathScanning to include additional scanners and improve controller method collection

--- src/main/java/com/poins/api/context/annotation/ClassPathScanning.java
+++ src/main/java/com/poins/api/context/annotation/ClassPathScanning.java
@@ -1,15 +1,32 @@
 package com.poins.api.context.annotation;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import org.reflections.Reflections;
 import org.reflections.scanners.Scanners;
 
 import com.poins.api.http.annotations.Controller;
 public class ClassPathScanning {
-    static public Reflections reflections = new Reflections("com.poins.api", Scanners.MethodsAnnotated, Scanners.TypesAnnotated);
+    static public final Reflections reflections = new Reflections("com.poins.api",
+    Scanners.MethodsAnnotated,
+    Scanners.TypesAnnotated,
+    Scanners.MethodsParameter,
+    Scanners.MethodsReturn
+    );
 
     public static Set<Class<?>> getAllControllerClasses() {
-        Set<Class<?>> controllers = ClassPathScanning.reflections.getTypesAnnotatedWith(Controller.class);
+        return ClassPathScanning.reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    public static Map<String, Method[]> collectControllers() {
+        Map<String, Method[]> controllers = new HashMap<String, Method[]>(); 
+        Set<Class<?>> classControllers = ClassPathScanning.getAllControllerClasses();
+        for(Class<?> controller : classControllers) {         
+                Method[] controllerMethods = controller.getDeclaredMethods();
+                controllers.put(controller.getName(), controllerMethods);
+        }
         return controllers;
     }
 }
