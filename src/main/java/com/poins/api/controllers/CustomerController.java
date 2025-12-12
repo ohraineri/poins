fd4d2602e1e7e7cb681369f0ff0a72fa244e50ed
Marcelo Raineri <mraineridev@gmail.com>
2025-12-11 23:56:17 -0300

    fix: reorder import statements and uncomment methods in CustomerController class

--- src/main/java/com/poins/api/controllers/CustomerController.java
+++ src/main/java/com/poins/api/controllers/CustomerController.java
@@ -1,35 +1,35 @@
 package com.poins.api.controllers;
 
-import com.poins.api.http.annotations.Controller;
-import com.poins.api.http.annotations.Delete;
 import com.poins.api.http.annotations.Get;
-import com.poins.api.http.annotations.Post;
 import com.poins.api.http.annotations.Put;
+import com.poins.api.http.annotations.Post;
+import com.poins.api.http.annotations.Delete;
+import com.poins.api.http.annotations.Controller;
 
 @Controller("/customer")
 public class CustomerController {
     @Get("/test")
     public void index() {
-
+        // Get Route
     }
 
     @Get("/")
     public void getAll() {
-
+        // Get Route
     }
 
-    // @Post("/")
-    // public void store() {
+    @Post("/")
+    public void store() {
+        // Post Route
+    }
 
-    // }
-
-    // @Put("/")
-    // public void update() {
-
-    // }
+    @Put("/")
+    public void update() {
+        // Update Route
+    }
     
-    // @Delete
-    // public void delete() {
-
-    // }
+    @Delete
+    public void delete() {
+        // Delete Route
+    }
 }
