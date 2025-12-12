2f1e240aaaa9c6e3c200e0a578216a96f6a2c5d3
Marcelo Raineri <mraineridev@gmail.com>
2025-12-11 23:46:58 -0300

    feat: refactor Server class to use bind method and initialize HttpHandler with collected controllers

--- src/main/java/com/poins/api/http/Server.java
+++ src/main/java/com/poins/api/http/Server.java
@@ -1,14 +1,25 @@
 package com.poins.api.http;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
+
+import com.poins.api.context.annotation.ClassPathScanning;
+import com.poins.api.context.annotation.HttpHandler;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpServer;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 public class Server {
-    static HttpServer server;
-    public void server(InetSocketAddress port){
+    public void bind(InetSocketAddress port) {
         try {
+            Map<String, Method[]> controllers = ClassPathScanning.collectControllers();
             HttpServer server = HttpServer.create(port, 0);
+            HttpHandler handler = new HttpHandler(controllers);
+            handler.registerRoutesFromMethods();
+            server.start();
         } catch(IOException e) {
             throw new Error(e);
         }
