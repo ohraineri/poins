71436b887b2c68c9624f609d22a8864a9d8f9a5e
Marcelo Raineri <mraineridev@gmail.com>
2025-12-25 23:38:08 -0300

    fix: update controllers map type from String to Class<?> for accurate route registration

--- src/main/java/com/poins/api/annotations/HttpHandler.java
+++ src/main/java/com/poins/api/annotations/HttpHandler.java
@@ -6,16 +6,16 @@ import com.poins.api.http.Server;
 import com.poins.api.http.annotations.Get;
 
 public class HttpHandler {
-    private Map<String, Method[]> controllers;
+    private Map<Class<?>, Method[]> controllers;
 
-    public HttpHandler(Map<String, Method[]> controller) {
+    public HttpHandler(Map<Class<?>, Method[]> controller) {
         this.controllers = controller;
     } 
 
     public void registerRoutesFromMethods() {
-        for(Map.Entry<String, Method[]> controller : this.controllers.entrySet()) {
+        for(Map.Entry<Class<?>, Method[]> controller : this.controllers.entrySet()) {
             for(int i = 0; i < controller.getValue().length; i++) {
-            Server.server.createContext(controller.getValue()[i].getAnnotation(Get.class).value(), new Handler());
+                Server.server.createContext(controller.getValue()[i].getAnnotation(Get.class).value(), new Handler());
             }
         }
     }
